3b993848214aff59ee7463aaebdc337883f93df6
Francis Oyitoba <oyitobafrancis@gmail.com>
2025-10-19 10:13:35 +0100

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL || 'http://localhost:5173',
-    'http://localhost:5174'
+    'http://localhost:5174', 'https://collab-board-teal.vercel.app'
   ],
   credentials: true
 }));
